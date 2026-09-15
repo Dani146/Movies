a81ef0fae39fc50855247d0aa2e442529db5b025
Dani146 <daniel.dub20092006@gmail.com>
2026-09-15 23:01:14 +0200

    get for actor id

--- backend/src/actors.py
+++ backend/src/actors.py
@@ -5,7 +5,10 @@ router = APIRouter()
 actors_list = {}
 
 
-@router.get("actors")
+@router.get("actors_list")
 def get_actor():
     return actors_list
 
+@router.get("/actor/{id}")
+def get_actor_id(id: int):
+    return actors_list[id]
