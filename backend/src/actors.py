62a59bc18a75db6c0c6e0954ea8738ae4d91b17a
Dani146 <daniel.dub20092006@gmail.com>
2026-09-15 23:02:35 +0200

    actors raise exception

--- backend/src/actors.py
+++ backend/src/actors.py
@@ -11,4 +11,11 @@ def get_actor():
 
 @router.get("/actor/{id}")
 def get_actor_id(id: int):
+
+    if id not in actors_list:
+        raise HTTPException(
+            status_code=404,
+            detail="actor was not found"
+        )
+
     return actors_list[id]
