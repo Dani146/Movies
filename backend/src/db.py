383c439e7752eecff6ec8ab04a4bc09da4f055c5
Dani146 <daniel.dub20092006@gmail.com>
2026-09-16 12:50:35 +0200

    basic backend setup

--- backend/src/db.py
+++ backend/src/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
 import datetime
-
+from sqlalchemy import Uuid
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
 class Base(DeclarativeBase):
@@ -16,8 +16,19 @@ class Base(DeclarativeBase):
 class Movie(Base):
     __tablename__ = "movies"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
     title = Column(String, nullable=False)
     year = Column(Integer, nullable=False)
     score = Column(Float, nullable=True)
     description = Column(String, nullable=False)
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
