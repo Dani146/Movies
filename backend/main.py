f631f07db7a32a343da701b6c9a845b6f8937dd6
Dani146 <daniel.dub20092006@gmail.com>
2026-09-16 13:02:18 +0200

    working setup of database

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI
 
 from src.app import router as movie_router
 from src.actors import router as actor_router
+from src.db import Movie, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(movie_router)
 app.include_router(actor_router)
