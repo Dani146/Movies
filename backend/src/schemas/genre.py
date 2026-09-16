3c44e5a0a82a3d2ce66f07a680f8fca620585ccc
Dani146 <daniel.dub20092006@gmail.com>
2026-09-16 12:04:48 +0200

    adding movies genre

---
+++ backend/src/schemas/genre.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class Genre(str, Enum):
+    ACTION = "action"
+    ADVENTURE = "adventure"
+    ANIMATION = "animation"
+    COMEDY = "comedy"
+    CRIME = "crime"
+    DOCUMENTARY = "documentary"
+    DRAMA = "drama"
+    FAMILY = "family"
+    FANTASY = "fantasy"
+    HISTORY = "history"
+    HORROR = "horror"
+    MUSIC = "music"
+    MYSTERY = "mystery"
+    ROMANCE = "romance"
+    SCI_FI = "sci-fi"
+    SPORT = "sport"
+    THRILLER = "thriller"
+    WAR = "war"
+    WESTERN = "western"
+    BIOGRAPHY = "biography"
