b6946c8d0db1165e7face6b445d1d89fde4c5e72
Dani146 <daniel.dub20092006@gmail.com>
2026-09-16 12:38:57 +0200

    table for a movie

--- backend/src/db.py
+++ backend/src/db.py
@@ -1,10 +1,11 @@
 from collections.abc import AsyncGenerator
 import uuid
-
+from sqlalchemy import Integer, Float
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
+import datetime
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
@@ -16,3 +17,7 @@ class Movie(Base):
     __tablename__ = "movies"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title = Column(String, nullable=False)
+    year = Column(Integer, nullable=False)
+    score = Column(Float, nullable=True)
+    description = Column(String, nullable=False)
--- backend/src/schemas/movie.py
+++ backend/src/schemas/movie.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
 from src.schemas.genre import Genre
+
 class MovieCreate(BaseModel):
     title: str
     year: int
     score: float
     description: str
-    genre: list[Genre]
+    genres: list[Genre]
