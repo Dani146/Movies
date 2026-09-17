5ec1a7eef56fe6b596456d24494db4fc74ede281
Dani146 <daniel.dub20092006@gmail.com>
2026-09-17 13:09:03 +0200

    comments and ehancing code

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, HTTPException
 from src.schemas.movie import MovieCreate
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
-from src.db import get_async_session
 from uuid import UUID
 from src.db import Movie, get_async_session
 from sqlalchemy import select
@@ -10,29 +9,28 @@ from sqlalchemy import select
 
 router = APIRouter()
 
+#  retrieves all Movie objects from the database
 @router.get("/movies_list")
 async def get_movie_list(session: AsyncSession = Depends(get_async_session)):
 
     result = await session.execute(select(Movie))
     movies = result.scalars().all()
     return movies
-
+# retrieve movie by its UUID id , raise error if there is no movie to be found in database
 @router.get("/movie/{id}")
 async def get_movie(id: UUID,
                     session: AsyncSession = Depends(get_async_session)):
 
-    movie_id = await session.get(Movie, id)
+    movie = await session.get(Movie, id)
 
-    if movie_id is None:
+    if movie is None:
         raise HTTPException(
             status_code=404,
             detail="movie was not found"
         )
 
-    return movie_id
-
-# add a post tomorrow
-
+    return movie
+# creates a movie entity and sends it to database and saves
 @router.post("/movies_list")
 async def create_movie(movie: MovieCreate,
                        session: AsyncSession = Depends(get_async_session)):
@@ -52,7 +50,7 @@ async def create_movie(movie: MovieCreate,
     return new_movie
 
    
-
+# takes a UUID and deletes the Movie with that primary key
 @router.delete("/movie/{id}")
 async def delete_movie(id: UUID,
                  session: AsyncSession = Depends(get_async_session)):
