cefe6d1e2a33f9bd4989127f76e9878871ee7a00
Dani146 <daniel.dub20092006@gmail.com>
2026-09-18 10:30:06 +0200

    post to add to database

--- backend/src/actors.py
+++ backend/src/actors.py
@@ -1,5 +1,11 @@
 from fastapi import APIRouter, HTTPException
-from src.schemas.actor import ActorCreate
+from src.schemas.movie import ActorCreate
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends
+from uuid import UUID
+from src.db import Actor, get_async_session
+from sqlalchemy import select
+
 router = APIRouter()
 
 actors_list = {}
@@ -23,7 +29,19 @@ def get_actor_id(id: int):
 
 
 @router.post("/actors_list")
-def add_actor(actor: ActorCreate):
-    actor_id = max(actors_list.keys(), default=0) + 1
-    actors_list[actor_id] = actor
-    return actor
+async def add_actor(actor: ActorCreate,
+              session: AsyncSession = Depends(get_async_session)):
+
+    new_actor = Actor(
+        name = actor.name,
+        surname = actor.surname,
+        birth_date = actor.birthdate
+    )
+
+    session.add(new_actor)
+
+    await session.commit()
+
+    await session.refresh(new_actor)
+
+    return new_actor
